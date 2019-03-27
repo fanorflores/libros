cb179f1c1c9de2d02e1d19956f4ccb0fee18afaf
Fanor Rivera <fanorflores@gmail.com>
2019-03-27 16:54:26 -0600

    quita cabecera

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php 
-include_once 'metadatos.php';
-include 'header.php'; 
-    $login=true;
-    $claseMenu= $login ? "col-lg-8":"col-lg-12";
-    $claseContent= $login ? "col-xs-6 col-sm-6 col-md-4 col-lg-3": "col-xs-6 col-sm-6 col-md-4 col-lg-2";
+
 ?>
 
 
